af7de2f681a28914536feb91174de607c8c377c7
vsky <blue205@centrum.cz>
2017-09-02 00:00:26 +0200

    Wolfgang Rosner's ping

--- app/include/user_config.h
+++ app/include/user_config.h
@@ -8,6 +8,7 @@
 // #define FLASH_8M
 // #define FLASH_16M
 #define FLASH_AUTOSIZE
+#define FLASH_SAFE_API
 
 // This adds the asserts in LUA. It also adds some useful extras to the
 // node module. This is all silent in normal operation and so can be enabled
@@ -21,7 +22,7 @@ extern void luaL_assertfail(const char *file, int line, const char *message);
 
 // This enables lots of debug output and changes the serial bit rate. This
 // is normally only used by hardcore developers
-// #define DEVELOP_VERSION
+#define DEVELOP_VERSION
 #ifdef DEVELOP_VERSION
 #define NODE_DEBUG
 #define COAP_DEBUG
@@ -111,6 +112,7 @@ extern void luaL_assertfail(const char *file, int line, const char *message);
 
 //#define WIFI_SMART_ENABLE
 
+#define WIFI_STATION_STATUS_MONITOR_ENABLE
 #define WIFI_SDK_EVENT_MONITOR_ENABLE
 #define WIFI_EVENT_MONITOR_DISCONNECT_REASON_LIST_ENABLE
 
