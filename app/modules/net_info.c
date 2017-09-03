69a8a4245796fe5220de51f558379bc6e2fb3fb0
vsky <blue205@centrum.cz>
2017-09-03 21:26:44 +0200

    Remove testing stuff

--- app/modules/net_info.c
+++ app/modules/net_info.c
@@ -27,6 +27,8 @@
 
 // https://github.com/nodemcu/nodemcu-firmware/wiki/[DRAFT]-How-to-write-a-C-module#debug-and-error-messages
 
+#define NET_INFO_DEBUG_ON
+
 #define log_prefix " ### DEBUG: net_info ### :  "
 
 #if defined(DEVELOP_VERSION)
@@ -44,102 +46,13 @@
 #endif
 
 
-
 #define NET_INFO_PRIORITY_OUTPUT   TASK_PRIORITY_MEDIUM
 #define NET_INFO_PRIORITY_ERROR    TASK_PRIORITY_MEDIUM
 
-
 // these statics should go away for reentrancy and maybe other reasons!
-static lua_State *gL = NULL;
 static int ping_callback_ref; 
 static int ping_host_count;
 static ip_addr_t ping_host_ip;
-
-
-// static task numbers for task API should be reentrant - I hope....
-//    do they contain a counter, right???
-static task_handle_t taskno_generic ;
-static task_handle_t taskno_error ;
-
-// task handling callbacks
-
-static void net_if_generic (task_param_t param, uint8_t prio ) {
-  (void) param;
-  (void) prio;
-
-  NET_INFO_DEBUG(" entering net_if_generic \n");
-  lua_State *L = lua_getstate();
-  NET_INFO_DEBUG("   ... in generic got lua state: 0x%x\n", L);
-// do what has to be done
-
-}
-
-static void net_if_error (task_param_t param, uint8_t prio ) {
-  // (void) param;
-  // (void) prio;
- 
-  NET_INFO_DEBUG(" entering net_if_error \n");
-  // get lua state (I'm supposed to be able in the task, right?)
-  lua_State *L = lua_getstate();
-  NET_INFO_DEBUG("   ... in error got lua state: 0x%x\n", L);
-
-  // cast param to char*
-  char *errmsg = (char*) param ;
-
-  // char errmsg[] = "lets try it with a dummy message";
-
-
-  NET_INFO_DEBUG("   ... in error got message: %s - pointers were 0x%x from 0x%x  \n", errmsg, errmsg , param );
- 
-  // raise lua error with message
-  lua_pushstring(L , errmsg);
-  NET_INFO_DEBUG("   ... pushed my msg to the stack...\n" );
-
-  lua_error(L);
-  NET_INFO_DEBUG("   ... and called a lua error (will I see this?)\n" );
- 
-}
-
-// register task handlers
-static int register_task_handlers(lua_State *L) {
-  NET_INFO_DEBUG("start registering task handlers\n");
-
-  taskno_generic = task_get_id(net_if_generic);
-  NET_INFO_DEBUG("   ...registered generic: 0x%x\n", taskno_generic);
-
-  taskno_error = task_get_id(net_if_error);
-  NET_INFO_DEBUG("   ...registered error 0x%x\n", taskno_error);
-  return 0;
-}
-
-// task handler test dummies
-static int net_info_err_task_dummy (lua_State *L) {
-  NET_INFO_DEBUG("entering task error dummy\n");
-  NET_INFO_DEBUG("   ... in dummy got lua state: 0x%x\n", L);
-  char msg[] = "I am the stupid hello task dummy";
-
-  NET_INFO_DEBUG("   and my message is %s\n", msg);
-  task_param_t param = (task_param_t) msg; 
-  NET_INFO_DEBUG("   have casted my param from 0x%x to 0x%x \n", msg, param);
-
-  task_post ( NET_INFO_PRIORITY_ERROR, taskno_error,  param );
-  NET_INFO_DEBUG("   ... have postet my msg task now... \n");
-}
-
-
-static int net_info_panic_dummy(lua_State *L) {
-  char errmsg[] = "Does lua_error really produce a panic?";
-  NET_INFO_DEBUG("   ... this is my panicking message: %s \n", errmsg );
-
-  // raise lua error with message
-  lua_pushstring(L , errmsg);
-  NET_INFO_DEBUG("   ... pushed my msg to the stack...\n" );
-  lua_error(L);
-  NET_INFO_DEBUG("   ... and called a lua error (will I see this?)\n" );
-}
-
-
-// ================================ start of old ping stuff ===================================
 
 void ping_received(void *arg, void *data) {
     // this would require change of the interface 
@@ -157,24 +70,23 @@ void ping_received(void *arg, void *data) {
     ipaddr_ntoa_r(&source_ip, ipaddrstr, sizeof(ipaddrstr));
 
     if (ping_callback_ref != LUA_NOREF) {
-      lua_rawgeti(gL, LUA_REGISTRYINDEX, ping_callback_ref);
-      lua_pushinteger(gL, pingresp->bytes);
-      lua_pushstring(gL, ipaddrstr);
-      lua_pushinteger(gL, pingresp->seqno);
-      lua_pushinteger(gL, pingresp->ttl);
-      lua_pushinteger(gL, pingresp->resp_time);
-      lua_call(gL, 5, 0);
+      lua_State *L = lua_getstate();
+      lua_rawgeti(L, LUA_REGISTRYINDEX, ping_callback_ref);
+      lua_pushinteger(L, pingresp->bytes);
+      lua_pushstring(L, ipaddrstr);
+      lua_pushinteger(L, pingresp->seqno);
+      lua_pushinteger(L, pingresp->ttl);
+      lua_pushinteger(L, pingresp->resp_time);
+      lua_call(L, 5, 0);
     } else {
       c_printf("%d bytes from %s, icmp_seq=%d ttl=%d time=%dms\n",
-	       pingresp->bytes,
-	       ipaddrstr,
-	       pingresp->seqno,
-	       pingresp->ttl,
-	       pingresp->resp_time);
+               pingresp->bytes,
+               ipaddrstr,
+               pingresp->seqno,
+               pingresp->ttl,
+               pingresp->resp_time);
     }
 }
-
-
 
 static void ping_by_hostname(const char *name, ip_addr_t *ipaddr, void *arg) {
     struct ping_option *ping_opt = (struct ping_option *)c_zalloc(sizeof(struct ping_option));
@@ -185,7 +97,7 @@ static void ping_by_hostname(const char *name, ip_addr_t *ipaddr, void *arg) {
     }
     if (ipaddr->addr == IPADDR_NONE) {
       c_printf("problem resolving hostname - maybe nonexistent host?\n");
-	return;
+        return;
     }
     
     ping_opt->count = ping_host_count;
@@ -200,19 +112,19 @@ static void ping_by_hostname(const char *name, ip_addr_t *ipaddr, void *arg) {
 /**
   * test.ping()
   * Description:
-  * 	Send ICMP ping request to address, optionally call callback when response received
+  *     Send ICMP ping request to address, optionally call callback when response received
   *
   * Syntax:
-  * 	wifi.sta.getconfig(ssid, password) --Set STATION configuration, Auto-connect by default, Connects to any BSSID
+  *     wifi.sta.getconfig(ssid, password) --Set STATION configuration, Auto-connect by default, Connects to any BSSID
   *     test.ping(address)              -- send 4 ping requests to target address
   *     test.ping(address, n)           -- send n ping requests to target address
   *     test.ping(address, n, callback) -- send n ping requests to target address
   * Parameters:
-  * 	address: string 
-  * 	n: number of requests to send
-  * 	callback: 
+  *     address: string 
+  *     n: number of requests to send
+  *     callback: 
   * Returns:
-  * 	Nothing.
+  *     Nothing.
   *
   * Example:
   *     test.ping("192.168.0.1")               -- send 4 pings to 192.168.0.1
@@ -228,14 +140,14 @@ static int net_info_ping(lua_State *L)
 
     // retrieve address arg (mandatory)
     if (lua_isstring(L, 1)) {
-	ping_target = luaL_checkstring(L, 1);
+        ping_target = luaL_checkstring(L, 1);
     } else {
-	return luaL_error(L, "no address specified");
+        return luaL_error(L, "no address specified");
     }
 
     // retrieve count arg (optional)
     if (lua_isnumber(L, 2)) {
-	count = luaL_checkinteger(L, 2);
+        count = luaL_checkinteger(L, 2);
     }
    
     // retrieve callback arg (optional)
@@ -246,47 +158,35 @@ static int net_info_ping(lua_State *L)
     if (lua_type(L, 3) == LUA_TFUNCTION || lua_type(L, 3) == LUA_TLIGHTFUNCTION)
       ping_callback_ref = luaL_ref(L, LUA_REGISTRYINDEX);
     
-    gL = L;   // global L
-
-
     // attempt to parse ping target as IP
     uint32 ip = ipaddr_addr(ping_target);
 
     if (ip != IPADDR_NONE) {
-	struct ping_option *ping_opt = (struct ping_option *)c_zalloc(sizeof(struct ping_option));
+        struct ping_option *ping_opt = (struct ping_option *)c_zalloc(sizeof(struct ping_option));
 
-	ping_opt->count = count;
-	ping_opt->ip = ip;
-	ping_opt->coarse_time = 0;
-	ping_opt->recv_function = &ping_received;
-	
-	ping_start(ping_opt);
-	
+        ping_opt->count = count;
+        ping_opt->ip = ip;
+        ping_opt->coarse_time = 0;
+        ping_opt->recv_function = &ping_received;
+        
+        ping_start(ping_opt);
+        
     } else {
-	ping_host_count = count;
+        ping_host_count = count;
 
-	struct espconn *ping_dns_lookup;
-	espconn_create(ping_dns_lookup);
-	espconn_gethostbyname(ping_dns_lookup, ping_target, &ping_host_ip, ping_by_hostname);	
+        struct espconn *ping_dns_lookup;
+        espconn_create(ping_dns_lookup);
+        espconn_gethostbyname(ping_dns_lookup, ping_target, &ping_host_ip, ping_by_hostname);   
     }
 
     return 0;
 }
 
-
-
 // Module function map
 static const LUA_REG_TYPE net_info_map[] = {
-  { LSTRKEY( "ping" ),             LFUNCVAL( net_info_ping ) },
-  { LSTRKEY( "dummy" ),            LFUNCVAL( net_info_err_task_dummy  ) },
-  { LSTRKEY( "panic" ),            LFUNCVAL (net_info_panic_dummy) },
-
-  { LSTRKEY( "__metatable" ),      LROVAL( net_info_map ) },
+  { LSTRKEY( "ping" ), LFUNCVAL( net_info_ping ) },
   { LNILKEY, LNILVAL }
 };
 
-
 // Register the module - NODEMCU_MODULE()  
-// NODEMCU_MODULE(NET_INFO, "net_info", net_info_map, NULL);
-NODEMCU_MODULE(NET_INFO, "net_info", net_info_map, register_task_handlers);
-
+NODEMCU_MODULE(NET_INFO, "net_info", net_info_map, NULL);
