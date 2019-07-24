f0cc7023eb7b4f72ecfc08866d10d339a791a207
vsky <blue205@centrum.cz>
2019-07-24 22:25:55 +0200

    Deployment configuration

--- app/include/user_modules.h
+++ app/include/user_modules.h
@@ -7,7 +7,7 @@
 // includes general purpose interface modules which require at most two GPIO pins.
 // See https://github.com/nodemcu/nodemcu-firmware/pull/1127 for discussions.
 // New modules should be disabled by default and added in alphabetical order.
-#define LUA_USE_MODULES_ADC
+// #define LUA_USE_MODULES_ADC
 //#define LUA_USE_MODULES_ADS1115
 //#define LUA_USE_MODULES_ADXL345
 //#define LUA_USE_MODULES_AM2320
@@ -19,12 +19,12 @@
 //#define LUA_USE_MODULES_BME680
 //#define LUA_USE_MODULES_COAP
 //#define LUA_USE_MODULES_COLOR_UTILS
-//#define LUA_USE_MODULES_CRON
-//#define LUA_USE_MODULES_CRYPTO
+#define LUA_USE_MODULES_CRON
+#define LUA_USE_MODULES_CRYPTO
 #define LUA_USE_MODULES_DCC
 #define LUA_USE_MODULES_DHT
 //#define LUA_USE_MODULES_ENCODER
-//#define LUA_USE_MODULES_ENDUSER_SETUP // USE_DNS in dhcpserver.h needs to be enabled for this module to work.
+#define LUA_USE_MODULES_ENDUSER_SETUP // USE_DNS in dhcpserver.h needs to be enabled for this module to work.
 #define LUA_USE_MODULES_FILE
 //#define LUA_USE_MODULES_GDBSTUB
 #define LUA_USE_MODULES_GPIO
@@ -33,14 +33,14 @@
 //#define LUA_USE_MODULES_HMC5883L
 //#define LUA_USE_MODULES_HTTP
 //#define LUA_USE_MODULES_HX711
-#define LUA_USE_MODULES_I2C
+// #define LUA_USE_MODULES_I2C
 //#define LUA_USE_MODULES_L3G4200D
 //#define LUA_USE_MODULES_MCP4725
 //#define LUA_USE_MODULES_MDNS
-#define LUA_USE_MODULES_MQTT
+// #define LUA_USE_MODULES_MQTT
 #define LUA_USE_MODULES_NET
 #define LUA_USE_MODULES_NODE
-#define LUA_USE_MODULES_OW
+// #define LUA_USE_MODULES_OW
 //#define LUA_USE_MODULES_PCM
 //#define LUA_USE_MODULES_PERF
 //#define LUA_USE_MODULES_PIPE
@@ -50,16 +50,16 @@
 //#define LUA_USE_MODULES_RFSWITCH
 //#define LUA_USE_MODULES_ROTARY
 //#define LUA_USE_MODULES_RTCFIFO
-//#define LUA_USE_MODULES_RTCMEM
-//#define LUA_USE_MODULES_RTCTIME
+#define LUA_USE_MODULES_RTCMEM
+#define LUA_USE_MODULES_RTCTIME
 //#define LUA_USE_MODULES_SI7021
 //#define LUA_USE_MODULES_SIGMA_DELTA
-//#define LUA_USE_MODULES_SJSON
-//#define LUA_USE_MODULES_SNTP
+#define LUA_USE_MODULES_SJSON
+#define LUA_USE_MODULES_SNTP
 //#define LUA_USE_MODULES_SOMFY
-#define LUA_USE_MODULES_SPI
+// #define LUA_USE_MODULES_SPI
 //#define LUA_USE_MODULES_SQLITE3
-//#define LUA_USE_MODULES_STRUCT
+#define LUA_USE_MODULES_STRUCT
 //#define LUA_USE_MODULES_SWITEC
 //#define LUA_USE_MODULES_TCS34725
 //#define LUA_USE_MODULES_TM1829
@@ -74,8 +74,8 @@
 //#define LUA_USE_MODULES_WIFI_MONITOR
 //#define LUA_USE_MODULES_WPS
 //#define LUA_USE_MODULES_WS2801
-//#define LUA_USE_MODULES_WS2812
-//#define LUA_USE_MODULES_WS2812_EFFECTS
+#define LUA_USE_MODULES_WS2812
+#define LUA_USE_MODULES_WS2812_EFFECTS
 //#define LUA_USE_MODULES_XPT2046
 
 //debug modules
